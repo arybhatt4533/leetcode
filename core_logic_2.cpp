19ca7ac2166c90ecdfe60417d34c4d1c83470382
arybhatt4533 <bhattarya4533@gmail.com>
2025-10-26 13:37:17 +0530

    Refactor: code optimization and formatting

--- core_logic_2.cpp
+++ core_logic_2.cpp
@@ -260,3 +260,4 @@
 // Verified update for 2025-10-26 13:37:17 - commit 5
 // Verified update for 2025-10-26 13:37:17 - commit 8
 // Verified update for 2025-10-26 13:37:17 - commit 11
+// Verified update for 2025-10-26 13:37:17 - commit 14
